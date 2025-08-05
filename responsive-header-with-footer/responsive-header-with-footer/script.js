fde76506720b1ca1dcc6a5fdd6bf1196ec525c7e
sreelakshmi <you@example.com>
2025-08-05 21:16:28 +0530

    created home page

---
+++ responsive-header-with-footer/responsive-header-with-footer/script.js
@@ -0,0 +1,15 @@
+// Load Header
+fetch("header.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("header").innerHTML = data;
+  });
+
+// Load Footer
+fetch("footer.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  });
+
+
---
+++ responsive-header-with-footer/responsive-header-with-footer/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./mainindex.html"],
+  theme: {
+    extend: {
+          fontFamily:{
+                Poppins:[ "poppins","sans-serif"],
+               
+            },
+    },
+  },
+  plugins: [],
+}
+
